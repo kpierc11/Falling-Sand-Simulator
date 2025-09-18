40dd78206ce3c0a25221cd013ca46681bcf7bd41
Emberwind-Studios <89319126+Emberwind-Studios@users.noreply.github.com>
2025-09-18 18:39:45 -0400

    Added Color Setting

--- src/SandSimulator.cpp
+++ src/SandSimulator.cpp
@@ -154,7 +154,11 @@ void SandSimulator::SimulationLoop()
 
 		ImGui::Text("Can change the color and particle style"); // Display some text (you can use a format strings too)
 		ImGui::Checkbox("Point Type", &particleSquare);			// Edit bools storing our window open/close state
-		
+		// ImGui::Checkbox("Another Window", &show_another_window);
+
+		// ImGui::SliderFloat("float", &f, 0.0f, 1.0f);			 // Edit 1 float using a slider from 0.0f to 1.0f
+		ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
+
 		ImGui::End();
 
 		Render();
